211146f661458ab786f7af0f2d9a42b9743a856c
Imoyemi <okeibrahim133@gmail.com>
2025-10-07 18:35:33 -0500

    feat: setup home page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import Image from 'next/image';
+import NavBar from './components/NavBar';
 
 export default function Home() {
-  return <div>portfolio</div>;
+  return (
+    <div className='font-instrument'>
+      <header>
+        <Image
+          src='/imotech-logo.svg'
+          width={200}
+          height={50}
+          alt='Imotech logo'
+        />
+        <NavBar />
+      </header>
+    </div>
+  );
 }
