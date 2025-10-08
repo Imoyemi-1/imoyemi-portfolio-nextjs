60d3670372a106ab6f9e639c8265cfc2ed4201f2
Imoyemi <okeibrahim133@gmail.com>
2025-10-07 20:28:18 -0500

    feat: install sheet from shadcn for mobile menu

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,27 @@
+'use client';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = (): void => setIsMobile(window.innerWidth < 768);
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    !isMobile && (
+      <nav>
+        <Link href={'/'}>Home</Link>
+        <Link href={'#services'}>Services</Link>
+        <Link href={'#about'}>About us</Link>
+        <Link href={'#portfolio'}>Portfolio</Link>
+        <Link href={'#contact'}>Contact</Link>
+      </nav>
+    )
+  );
 };
 
 export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import NavBar from './components/NavBar';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 
 export default function Home() {
   return (
     <div className='font-instrument'>
-      <header>
-        <Image
-          src='/imotech-logo.svg'
-          width={200}
-          height={50}
-          alt='Imotech logo'
-        />
+      <header className='p-4 flex items-center justify-between'>
+        <Link href={'/'}>
+          <Image
+            className='w-full max-w-35'
+            src='/imotech-logo.svg'
+            width={200}
+            height={50}
+            alt='Imotech logo'
+          />
+        </Link>
         <NavBar />
+        <HiOutlineMenuAlt3 className='w-6 h-6 cursor-pointer' />
       </header>
     </div>
   );
